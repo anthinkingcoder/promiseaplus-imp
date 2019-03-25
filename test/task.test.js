c834ca9b297b539a7187c2b84b546dccbbdf33d2
周琳 <zhoul@3vjia.com>
2019-03-25 14:12:32 +0800

    test: add asyncTask test

---
+++ test/task.test.js
@@ -0,0 +1,18 @@
+const Promise = require('../');
+setTimeout(function () {
+  console.log('c');
+})
+const promise = new Promise(function (resolve, reject) {
+  resolve('b');
+}).then(function (value) {
+  console.log(value);
+  return value;
+}).then(function (value) {
+  console.log(value)
+  return Promise.reject(value)
+}).catch(function (reason) {
+  console.log(reason)
+});
+
+console.log('a');
+
